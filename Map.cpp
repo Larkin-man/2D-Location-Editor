2c873f8364a2caea026d150a91d67b3d2c9920d2
Larkin <ab88199594ae@mail.ru>
2022-11-24 02:28:51 +0400

    tilesize 1024
    Update TMap:
    typedef char
    vector
    4 chars
    sprites add
    unicodestring to string

--- Map.cpp
+++ Map.cpp
@@ -1,39 +1,63 @@
 //---------------------------------------------------------------------------
 //#include <ustring.h>
-#include <System.hpp>
+//#include <System.hpp>
 #include <stdio.h>
 #include <Graphics.hpp>
+
+//#include <TList>
 //#include <SysUtils.hpp>
 #pragma hdrstop
 #include "Map.h"
 //-------------------------------------------------------------------
-int TMap::GetLocationIndex(char ident)
+int TMap::GetLocationIndex(const tileid ident) const
 {
 	for (int i = 0; i < TilesCount; ++i)
-		if (Idents[i] == ident)
+		//if (Idents[i] == ident)
+		if (Idents[i][0] == ident[0]&&Idents[i][1] == ident[1]
+			&&Idents[i][2] == ident[2]&&Idents[i][3] == ident[3])
 			return i;
 	return UndefindedId;
 }
 //-------------------------------------------------------------------
 __fastcall TMap::TMap()
 {
-	FHeight = FWidth = 0;
+	FHeight = 0;
+	FWidth = 0;
 	Cell = NULL;
 	UndefindedId = 0;
 	file = NULL;
 	DebugOpenTrig = false;
+	Idents = NULL;
+	SpritesCount = 0;
+	SpriteNames = NULL;
+	Buttons = NULL;
+	Variants = NULL;
+	//SpriteSizes = NULL;
+	Sprites = NULL;
 }
 //-------------------------------------------------------------------
 __fastcall TMap::~TMap()
 {
 	delete file;
+	delete [] Idents;
 	Destroy();
+	delete SpriteNames;
+	delete [] Buttons;
+	delete [] Variants;
+	//delete [] SpriteSizes;
+	for (int i = 0; i < SpritesCount; ++i)
+		delete Sprites[i];
+	delete [] Sprites;
 }
 //-------------------------------------------------------------------
 void TMap::Destroy()
 {
 	for (int i = 0; i < FWidth; i++)
+	{
+		//for (int j = 0; j < FHeight; j++)
+		//	delete [] Cell[i][j].Sprites;
 		delete [] Cell[i];
+	}
 	delete [] Cell;
 }
 //-------------------------------------------------------------------
@@ -50,21 +74,23 @@ void TMap::Create(int Width, int Height)
 //-------------------------------------------------------------------
 void TMap::Clear()
 {
-	int nl = GetLocationIndex('0');
+	int nl = GetLocationIndex(NoneTileStr);
 	for(int i = 0; i < Width; i++)
 		for(int j = 0; j < Height; j++)
 		{
 			Cell[i][j].Image = nl;
-			Cell[i][j].Ident = '0';
+			//_wcsnset(Cell[i][j].Ident, L'_', SYMBCOUNT);
+			strncpy(Cell[i][j].Ident, NoneTileStr, SYMBCOUNT);
 			Cell[i][j].Radiation = 0;
 			Cell[i][j].Anomally = 0;
 			Cell[i][j].Triggger = 0;
+			Cell[i][j].Variant = 0;
 		}
 }
 //-------------------------------------------------------------------
-bool TMap::Open(UnicodeString FileName)
+bool TMap::Open(String FileName)
 {
-	FILE *fp = fopen (FileName.c_str(), "r");
+	FILE *fp = fopen(FileName.c_str(), "rb");
 	if (!fp)
 	{
 		fclose(fp);
@@ -73,21 +99,22 @@ bool TMap::Open(UnicodeString FileName)
 	int W, H;
 	fread(&W, sizeof(int), 1, fp);
 	fread(&H, sizeof(int), 1, fp);
-	if ((W>1000)||(H>1000)||(W<0)||(H<0))
+	if ((W>9000)||(H>9000)||(W<0)||(H<0))
 	{
 		fclose(fp);
 		throw (ERangeError("Out of range"));
 	}
 	Map.Create(W, H);
-
 	for (int i = 0; i < Map.FWidth; ++i)
 		for (int j = 0; j < Map.FHeight; ++j)
 		{
-			fread(&Map.Cell[i][j].Ident, sizeof(char), 1, fp);
+			fread(Map.Cell[i][j].Ident, sizeof(char), SYMBCOUNT, fp);
+			  //fread(Map.Cell[i][j].Ident, sizeof(char), 3, fp);
+			  //Map.Cell[i][j].Ident[1] = '\0';
+			  //Map.Cell[i][j].Variant = 0;
+			fread(&Map.Cell[i][j].Variant, 1, 1, fp);
 			fread(&Map.Cell[i][j].Radiation, sizeof(unsigned char), 1, fp);
 			fread(&Map.Cell[i][j].Anomally, sizeof(unsigned char), 1, fp);
-			Map.Cell[i][j].Radiation -= OSHIFT;
-			Map.Cell[i][j].Anomally -= OSHIFT;
 			Map.Cell[i][j].Image = GetLocationIndex(Map.Cell[i][j].Ident);//   ConvertIdent();
 			Map.Cell[i][j].ColorAnom = GetAnomallyColor(Map.Cell[i][j].Anomally);
 			Map.Cell[i][j].ColorRad = GetRadiationColor(Map.Cell[i][j].Radiation);
@@ -95,24 +122,59 @@ bool TMap::Open(UnicodeString FileName)
 		}
 	if (DebugOpenTrig)
 	{
+		tileidnl sect;
+		fread(&sect, sizeof(char), 5, fp);
 		int TriggersCount = 0;
 		fread(&TriggersCount, sizeof(int), 1, fp);
-		//int c,r;
 		for (int i = 0; i < TriggersCount; ++i)
 		{
 			fread(&W, sizeof(int), 1, fp);
 			fread(&H, sizeof(int), 1, fp);
-			fread(&Map.Cell[W-OSHIFT][H-OSHIFT].Triggger, sizeof(int), 1, fp);
-			Map.Cell[W-OSHIFT][H-OSHIFT].Triggger -= OSHIFT;
+			fread(&Map.Cell[W][H].Triggger, 1, 1, fp);
 		}         //*/
+		//Загрузим спрайты уважаемый сэр
+
+		fread(&sect, sizeof(char), 5, fp);
+		int sprc = 0, sprs = 0;
+		fread(&sprc, sizeof(int), 1, fp);
+		tileid sprt;
+		for (int i = 0; i < sprc; ++i)
+		{
+			fread(sprt, sizeof(char), SYMBCOUNT, fp);
+			fread(&sprs, sizeof(int), 1, fp);
+			int x=0, y=0, sprind=-1;
+			if (SpriteNames)//&&((in = Sprites->Names. IndexOf(sprt)) > -1))
+			{
+				for (int j = 0;j < SpriteNames->Count; ++j)
+					if (strncmp(SpriteNames->Names[j].c_str(), sprt, SYMBCOUNT) == 0)
+					{
+						sprind = j;
+						break;
+					}
+				if (sprind == -1)
+					continue;    //*/
+				for (int i = 0; i < sprs; ++i)
+				{
+					fread(&x, sizeof(int), 1, fp);
+					fread(&y, sizeof(int), 1, fp);
+					if (Sprites)
+						SpritesList.push_back(TSprite(sprind,x,y));
+//					if (Sprites) //
+//						Cell[x/TILESIZE][y/TILESIZE].Sprites.push_back(TSprite(sprind,x%TILESIZE-Sprites[sprind]->Width,y%TILESIZE-Sprites[sprind]->Height));
+//					else
+//						Cell[x/TILESIZE][y/TILESIZE].Sprites.push_back(TSprite(sprind,x%TILESIZE,y%TILESIZE));
+				}
+
+			}
+		}    //*/
 	}
 	fclose(fp);
 	return true;
  }
 //-------------------------------------------------------------------
-bool TMap::Save(UnicodeString FileName)    //Функция сохраняет список в бинарный файл
+bool TMap::Save(String FileName)    //Функция сохраняет список в бинарный файл
 {
-	FILE *fp = fopen (FileName.c_str(), "w");
+	FILE *fp = fopen (FileName.c_str(), "wb");
 	if (!fp)
 	{
 		fclose(fp);
@@ -121,36 +183,75 @@ bool TMap::Save(UnicodeString FileName)    //Функция сохраняет список в бинарный
 	}
 	fwrite(&FWidth, sizeof(int), 1, fp);
 	fwrite(&FHeight, sizeof(int), 1, fp);
-	unsigned char R,A;
 	int TriggersCount = 0;
 	for (int i = 0; i < Map.FWidth; ++i)
 		for (int j = 0; j < Map.FHeight; ++j)
 		{
-			fwrite(&Map.Cell[i][j].Ident, sizeof(char), 1, fp);
-			R = Map.Cell[i][j].Radiation + SSHIFT;
-			A = Map.Cell[i][j].Anomally + SSHIFT;
-			fwrite(&R, sizeof(unsigned char), 1, fp);
-			fwrite(&A, sizeof(unsigned char), 1, fp);
+			//strncpy(Map.Cell[i][j].Ident, Map.Idents[Map.Cell[i][j].Image], SYMBCOUNT);
+			if (Map.Cell[i][j].Image < UndefindedId)
+				fwrite(Idents[Map.Cell[i][j].Image], sizeof(char), SYMBCOUNT, fp);
+			else
+				fwrite(Map.Cell[i][j].Ident, sizeof(char), SYMBCOUNT, fp);
+			  //fwrite(file->Values[Map.Cell[i][j].Ident].c_str(), sizeof(char), SYMBCOUNT, fp);
+			fwrite(&Map.Cell[i][j].Variant, 1, 1, fp);
+			fwrite(&Map.Cell[i][j].Radiation, sizeof(unsigned char), 1, fp);
+			fwrite(&Map.Cell[i][j].Anomally, sizeof(unsigned char), 1, fp);
 			if (Map.Cell[i][j].Triggger > 0)
 				TriggersCount++;
 		}
 
 	if (DebugOpenTrig)
 	{
-		unsigned int I,J,T;
+		fwrite("TRIGS", sizeof(char), 5, fp);
 		fwrite(&TriggersCount, sizeof(int), 1, fp);
 		for (int i = 0; i < Map.FWidth; ++i)
 			for (int j = 0; j < Map.FHeight; ++j)
 				if (Map.Cell[i][j].Triggger > 0)
 				{
-					I = i + SSHIFT; J = j + SSHIFT;
-					T = Map.Cell[i][j].Triggger + SSHIFT;
-					fwrite(&I, sizeof(int), 1, fp);
-					fwrite(&J, sizeof(int), 1, fp);
-					fwrite(&T, sizeof(int), 1, fp);
-					//T = Map.Cell[i][j].Triggger + SSHIFT;
-					//fwrite(&T, sizeof(unsigned char), 1, fp);
+					fwrite(&i, sizeof(int), 1, fp);
+					fwrite(&j, sizeof(int), 1, fp);
+					fwrite(&Map.Cell[i][j].Triggger, 1, 1, fp); //DONE:1byte
 				}
+
+		//Сохраним ка спрайты уажаемый сэр
+		//TODO:peredelat cherez bazovyi vector
+		std::vector<std::vector<MyPoint> >Saves(SpritesCount);
+		//for (int i = 0; i < SpritesCount; ++i)
+		//Saves = new std::vector<MyPoint>[SpritesCount];
+		//for (int i = 0; i < Map.FWidth; ++i)
+		//	for (int j = 0; j < Map.FHeight; ++j)
+		for (std::vector<TSprite>::iterator el = SpritesList.begin(); el != SpritesList.end(); ++el)
+			{
+				//if (Map.Cell[i][j].Sprites.size() > 0)
+					//for (std::vector<TSprite>::iterator el = Map.Cell[i][j].Sprites.begin(); el != Map.Cell[i][j].Sprites.end(); ++el)
+			Saves[el->Index].push_back(MyPoint(el->x,el->y));
+			}
+
+		int sc = 0;
+		for (std::vector<std::vector<MyPoint> >::iterator el = Saves.begin(); el != Saves.end(); ++el)
+			if (el->size() > 0)
+				sc++;
+		fwrite("SPRTS", sizeof(char), 5, fp);
+		fwrite(&sc, sizeof(int), 1, fp);
+		if (sc > 0)
+		for (int v = 0; v < SpritesCount; ++v)
+		//for (std::vector<std::vector<MyPoint> >::iterator V = Saves.begin(); V != Saves.end(); ++V)
+		{
+			if (Saves[v].size() <= 0)
+				continue;
+			fwrite(SpriteNames->Names[v].c_str(), sizeof(char), SYMBCOUNT, fp);
+			int ZAEBALA = Saves[v].size();
+			fwrite(&ZAEBALA, sizeof(int), 1, fp);
+			for (std::vector<MyPoint>::iterator el = Saves[v].begin(); el != Saves[v].end(); ++el)
+			{
+				int x = el->x+Sprites[v]->Width;
+				int y = el->y+Sprites[v]->Height;
+				fwrite(&x, sizeof(int), 1, fp);
+				fwrite(&y, sizeof(int), 1, fp);
+			} //*/
+		}
+		//delete [] Saves;
+
 	}
 	fclose(fp);
 	return true;
@@ -252,7 +353,7 @@ void TMap::AnomallyRun() //[W][H]
 		}
 }
 //-------------------------------------------------------------------
-void TMap::LoadTiles(const UnicodeString FileName)
+void TMap::LoadTiles(const String FileName)
 {
 	file = new TStringList;
 	try
@@ -262,15 +363,67 @@ void TMap::LoadTiles(const UnicodeString FileName)
 	{
 		throw;
 	}
+	for (int i = 0; i < file->Count; ++i)
+		if (unsigned(file->Strings[i].Length()) <= SYMBCOUNT)
+		{
+			file->Delete(i);
+			i--;
+		}
 	TilesCount = file->Count;
-	Idents = new wchar_t[TilesCount];
+	Idents = new tileidnl[TilesCount];
+	tileid Group = {'0','0','0','0'};
+	int nGroup = 0;
+	Variants = new int[file->Count+1];
 	for (int i = 0; i < TilesCount; i++)
 	{
-		Idents[i] = file->Names[i][1];
+		strncpy(Idents[i], file->Names[i].c_str(), SYMBCOUNT);
+		Idents[i][SYMBCOUNT] = '\0';
+		//Для группировки тайлов
+		if (strncmp(Group, Idents[i], SYMBCOUNT-1) == 0 && Idents[i][SYMBCOUNT-1]>='0'&&Idents[i][SYMBCOUNT-1]<='9')
+		{  //одинаковы и 4-цифра
+			nGroup++;
+			Variants[i] = 1;
+		}
+		else
+		{
+			strncpy(Group, Idents[i], SYMBCOUNT-1);
+			Variants[i] = 0;
+		}
+
+		//Idents[i][0] = file->Names[i][1];
 		//Maps.Name[i] = file->Values[i+1];
-		//Memo1->Lines->Add(Maps.Ident[i]);
-		//Memo1->Lines->Add(Maps.Name[i]);
-		//Memo1->Lines->Add(file->Values[Maps.Ident[i]]);
 	}
+	Variants[file->Count] = 0;
+	Buttons = new int[file->Count-nGroup];
+	file->Add("Хрен знает что");
+	UndefindedId = TilesCount;//GetLocationIndex(NoneTileStr);
 }
+//-------------------------------------------------------------------
+void TMap::LoadSprites(const UnicodeString FileName)
+{
+	SpriteNames = new TStringList;
+	SpriteNames->LoadFromFile(FileName);//"Sprites\\SPRITES.txt");
+	for (int i = 0; i < SpriteNames->Count; ++i)
+		if (unsigned(SpriteNames->Strings[i].Length()) <= 4)
+		{
+			SpriteNames->Delete(i);
+			i--;
+		}
+	SpritesCount = SpriteNames->Count;
+	Sprites = new TPicture*[SpritesCount];
+	//Sprites = new TImage*[SpritesCount];
+	//Idents = new tileidnl[TilesCount];
+	for (int i = 0; i < SpritesCount; ++i)
+	{
+		Sprites[i] = new TPicture;
+		Sprites[i]->Bitmap->AlphaFormat = afDefined;
+		Sprites[i]->Bitmap->LoadFromFile("Sprites\\"+Map.SpriteNames->Names[i]+".bmp");// = Image3->Picture;
+      //Sprites[i]->Bitmap->Transparent = true; //Это пинзевс
+
+	}
+	//SpriteSizes = new MyPoint[SpritesCount];
+
+}
+//-------------------------------------------------------------------
+
 //-------------------------------------------------------------------
--- Map.h
+++ Map.h
@@ -1,10 +1,15 @@
 //---------------------------------------------------------------------------
 #ifndef MapH
 #define MapH
+#include <vector>
 //---------------------------------------------------------------------------
-int const METERS = 40;
-unsigned char const OSHIFT = 32;
-unsigned char const SSHIFT = 32;
+const size_t SYMBCOUNT = 4;
+typedef char tileid[SYMBCOUNT];
+typedef char tileidnl[SYMBCOUNT+1];
+tileid const NoneTileStr = {"NONE"};
+const int TILESIZE = 1024;
+
+//const tileid NoneTile = {L"NONE"}; //TODO: рисуй его
 
 TColor SetRGBint(int Red, int Green, int Blue)//макс 255
 {
@@ -18,34 +23,67 @@ inline int TransformNumber(int InitLeft, int FinalLeft, int InitRight, int Final
 	return ((FinalRight-FinalLeft)*(Numb-InitLeft))/double(InitRight-InitLeft) + FinalLeft;
 }
 
+struct MyPoint
+{
+	int x,y;
+	MyPoint(int l, int t) {x=l;y=t; }
+	//MyPoint() { }
+};
+
 class TMap
 {
 private:
 	int FHeight, FWidth;
-	wchar_t *Idents;
-	TStringList *file;
 protected:
-	wchar_t GetTile(int i) const	{  return Idents[i];   }
-	UnicodeString GetTileName(int i) { return file->Values[Idents[i]]; }
+	tileidnl* GetTile(int i) const	{  return &Idents[i];   }
+	String GetTileName(int i)
+	{
+	//char dd[5];
+	//strncpy(dd, Idents[i], SYMBCOUNT);
+	//dd[4] = '\0';
+	 //String gg = file->Names[i];
+	 //String ff =file->Values[dd];//нужен нулл в конце
+	 //return ff;
+	return 	file->Values[Idents[i]];
+	}
 public:
-
+	tileidnl *Idents;
+	TStringList *file;
+	struct TSprite
+	{
+		//tileid Ident;
+		TSprite(int index, int left, int top)
+		{
+			Index = index;
+			x = left;
+			y = top;
+      }
+		int Index;
+		int x,y;   //Координаты верхнего левого угла
+	} ;
 	typedef struct
 	{
 		int Image;
-		wchar_t Ident;
+		tileid Ident;
 		unsigned char Radiation;
 		unsigned char Anomally;
+		int Variant;
+		//Not save:
 		TColor ColorRad;
 		TColor ColorAnom;
+		int Triggger;
+		bool AnomTest;
+		//TSprite *Sprites;
+		std::vector<TSprite> Sprites;
+		//int SprCount;
 		///void ConvertIdent()	{	Image = GetLocationIndex(Ident); }
 		void SetRadiation(int iRadiation);
 		void SetAnomally(int iAnomally);
-		bool AnomTest;
-		int Triggger;
+		void DeleteInPos(int x, int y);
 	} TCell;
 	void Create(int Width, int Height);    //init
 	void Clear();
-	__property int Width = {read=FWidth};
+	__property int Width = {read=FWidth}; //0 - W-1
 	__property int Height = {read=FHeight};
 	TCell **Cell; //[W][H]
 	bool DebugOpenTrig;
@@ -60,10 +98,19 @@ public:
 	//Base Tiles
 	void LoadTiles(const UnicodeString FileName);
 	int TilesCount;
-	__property wchar_t TileIdent[int i] = {read=GetTile};
-	__property UnicodeString TileName[int i] = {read=GetTileName};
-	int GetLocationIndex(char ident);
+	__property tileidnl TileIdent[int i] = {read=GetTile};
+	__property String TileName[int i] = {read=GetTileName};
+	int GetLocationIndex(const tileid ident) const;
 	int UndefindedId;
+	int *Buttons; //При клике на кнопку тайла
+	int *Variants; //При смене варианта указывает следующий
+	//Спрайты
+	TStringList *SpriteNames;
+	int SpritesCount;
+	void LoadSprites(const UnicodeString FileName);
+	//MyPoint *SpriteSizes;
+	TPicture **Sprites;
+	std::vector<TSprite> SpritesList;
 };
 extern TMap Map;
 
